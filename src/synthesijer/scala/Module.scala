b716d5ff7d1d6f5371880167a8c663883bb48bb6
Takefumi MIYOSHI <miyo@wasamon.net>
2014-10-31 10:04:35 +0900

    bugfix: >>>, <<

--- src/synthesijer/scala/Module.scala
+++ src/synthesijer/scala/Module.scala
@@ -180,8 +180,8 @@ abstract class ExprItem(val module:ModuleFunc) {
 	def & (e:ExprItem):ExprItem = module.expr(Op.concat, this, e)
 	def concat (e:ExprItem):ExprItem = module.expr(Op.&, this, e)
 	def >> (v:Int):ExprItem = module.expr(Op.>>, this, v)
-	def >>> (v:Int):ExprItem = module.expr(Op.>>, this, v)
-	def << (v:Int):ExprItem = module.expr(Op.>>, this, v)
+	def >>> (v:Int):ExprItem = module.expr(Op.>>>, this, v)
+	def << (v:Int):ExprItem = module.expr(Op.<<, this, v)
   
 }
 

63b566ba395856517ec6a118fbc673b2e8567011
Takefumi MIYOSHI <miyo@wasamon.net>
2015-11-23 20:20:16 +0900

    to use dual-clocks in ExtFifo

--- src/main/scala/synthesijer/scala/utils/ExtFifo.scala
+++ src/main/scala/synthesijer/scala/utils/ExtFifo.scala
@@ -2,7 +2,7 @@ package synthesijer.scala.utils
 
 import synthesijer.scala._
 
-class ExtFifo(name:String, width:Int) extends Module(name, "clk", "srst"){
+class ExtFifo(name:String, width:Int) extends Module(name, "rd_clk", "rst"){
 
   val din   = inP("din", width)
   val dout  = outP("dout", width)
@@ -10,5 +10,7 @@ class ExtFifo(name:String, width:Int) extends Module(name, "clk", "srst"){
   val we    = inP("wr_en")
   val empty = outP("empty")
   val full  = outP("full")
+  val prog_full  = outP("prog_full")
+  val wr_clk = inP("wr_clk")
 
 }
